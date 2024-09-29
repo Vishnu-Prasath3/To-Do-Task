4494a51d15525438b5cdce4e3039b87abb9188f9
vishnuprasath03 <eeevishnuprasath@gmail.com>
2024-09-29 21:55:11 +0530

    array maped to usestate

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
 import {useState} from 'react'
 
 function Todoform() {
-    const[state,setstate]=useState([{task:"",decription:""}])
-
-const handlsubmit=(e)=>{
+    const[state,setstate]=useState([])
+    let data=[...state];
+    //  [data]=[...state];
+    const handlsubmit=(e)=>{
     e.preventDefault();
-
-}
+    // console.log(e.target.name.value);
+    // console.log(e.target.desc.value);
+    data.push({name:e.target.desc.value,desc:e.target.name.value});
+    e.target.name.value="";
+    e.target.desc.value="";
+    setstate(data);
+    }
 
   return (
     <>
     <div>
         <h1>My Todo</h1>
-        <form onSubmit={ handlsubmit}>
-            <input type='text' ></input>
-            <input type='text'></input>
-            <button type='submit'>Add ToDO</button>
+        <form onSubmit={handlsubmit}>
+            <input type='text' required name='name' placeholder='To-Do-Name'></input>
+            <input type='text' required name='desc' placeholder='To-Do-Description'></input>
+            <button type='submit' >Add ToDO</button>
         </form>
     </div>
     </>
